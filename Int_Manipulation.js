263b9e379faf2910190a03760fc06a4f99de9965
Wajdan-Faheem <wajdanf02@gmail.com>
2021-05-04 03:34:29 -0400

    added int manup

--- Int_Manipulation.js
+++ Int_Manipulation.js
@@ -20,3 +20,28 @@ console.log(likes);
 
 likes+=3; //or likes++; for only adding 1
 console.log(likes);
+
+//NaN - Not a Number
+//Ex. console.log(5*'hello')
+
+let results = "the blog has"+ " "+ likes +" " + "likes";
+console.log(results);
+
+//Concatenation way like above
+const title = 'lol';
+const author='wajdan'
+const like = 40;
+ result = "the blog called "+title+' by '+author+ ' has '+like+' likes'
+ console.log(result)
+
+ //templates string way <- prefer this way
+ result = `The blog called ${title} by ${author} has ${like} likes`
+ console.log(result);
+
+ //creating html templates
+ let html = `
+<h2>${title} </h2>
+<p>The Author is ${author} </p>
+ `;
+ console.log(html);
+
