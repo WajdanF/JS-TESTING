b6cbaa3fecbf227b1ea2ef973aee77b261f329e9
Wajdan-Faheem <wajdanf02@gmail.com>
2021-05-05 15:14:13 -0400

    adding

---
+++ functions.js
@@ -0,0 +1,11 @@
+//funcion dec
+
+function greet(){
+    console.log("hello");
+}
+greet(); //calling the func
+
+//function expression
+const speak = function(){
+    console.log("expression");
+};
