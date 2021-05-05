fcd360655d7c2d19ce358fcc40ad906c10d1a5d2
Wajdan-Faheem <wajdanf02@gmail.com>
2021-05-04 21:52:25 -0400

    conditional stuff

--- loops.js
+++ loops.js
@@ -6,5 +6,22 @@ for(let i=0; i<5;i++){ //setting up a forloop -> (starting, condition,step)
 const names = ['wajdan','phil','mashrur']
 for (let i = 0; i < names.length; i++) {
     console.log(i,names[i]);
-    
+    let html=`<div>${names[i]}</div>`
+    console.log(html);
 }
+
+//while loops
+let i=0;
+while(i < names.length){ //condition
+    console.log(names[i],'name');
+    i++;
+}
+
+//do while -> will run the loop atleast once
+i=5
+do{
+   console.log(i); 
+}
+while(i<5);
+
+
