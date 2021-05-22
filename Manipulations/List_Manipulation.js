7a247da6b5d532dcf20c715be95d9863e9bc636f
Wajdan-Faheem <wajdanf02@gmail.com>
2021-05-22 01:38:01 -0400

    list manipulations

--- Manipulations/List_Manipulation.js
+++ Manipulations/List_Manipulation.js
@@ -38,6 +38,61 @@ x.forEach((i, X) => {
 
 //Filtering through a list
 const score = [10, 20, 30, 15, 19, 12, 13, 43];
-score.filter(() => {
-    return true;//if returned true, then keep that item in the list, else remove it
+const filteredScore= score.filter((i) => {// does not change the actual array
+
+    return i>20;//if returned true, then keep that item in the list, else remove it
 });
+console.log(filteredScore); 
+
+const users = [
+    { name: "shaun", premium: true },
+    { name: "yoshi", premium: false },
+    { name: "mario", premium: false },
+    { name: "chun-li", premium: true },
+];
+  
+const premiumUsers = users.filter(user=>{
+    return user.premium
+})
+console.log(premiumUsers);
+
+//MAPPING AN ARRAY
+const prices = [20,10,30,40,55,15,60]
+
+const salePrices = prices.map((price)=>{ //creates a new array based on the previous array
+    return price/2;
+})
+
+console.log(salePrices);
+
+const products = [
+    { name: "gold star", price: 20 },
+    { name: "mushroom", price: 40 },
+    { name: "green shells", price: 30 },
+    { name: "banana skin", price: 10 },
+    { name: "red shells", price: 50 },
+];
+
+const sales = products.map(item=>{
+    if (item.price >30){
+        return {name:item.name,price:item.price/2}
+    }
+    else{
+        return item
+    }
+})
+console.log(sales);
+
+//REDUCING AN ARRAY -> returns a number for the expression passed
+const scores = [10,20,304,05,303,432,432,432,4]
+const reduced = score.reduce((acc,curr)=>{//acc = running total, curr = current item
+    if(curr <50){
+        acc++;
+    }
+    return acc;
+
+},0);//intial value of the acc is second argument
+
+//SORTING
+scores.sort((a,b)=>a-b); 
+
