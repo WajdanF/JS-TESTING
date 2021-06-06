b103cea136a5777bbc8b255103186ce8316237c7
Wajdan-Faheem <wajdanf02@gmail.com>
2021-06-06 03:30:01 -0400

    loggs

--- Firebase_Auth/scripts/auth.js
+++ Firebase_Auth/scripts/auth.js
@@ -1,59 +1,83 @@
+//LISTENING FOR AUTH status CHANGES -> Actively checking
+
+auth.onAuthStateChanged((user) => {
+    //user is null if person is signed out otherwise its the user object
+    console.log(user);
+    if (user) {
+        //if user is logged in
+        //Getting data
+        db.collection("guides").get()
+            .then((snapshot) => {
+                setupGuides(snapshot.docs); 
+                setupUI(user)
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    } else {
+        //if user is logged out
+        setupGuides([]);    
+
+        setupUI();
+    }
+});
+
 //sign up
 
 const signup = document.querySelector("#signup-form");
 
 //SIGING UP
-    signup.addEventListener("submit", (e) => {
+signup.addEventListener("submit", (e) => {
     e.preventDefault();
     const email = signup["signup-email"].value;
     const password = signup["signup-password"].value;
 
     //singing up the user using firebase
-    auth.createUserWithEmailAndPassword(email, password) //this functino returns the credentials of a user  
+    auth.createUserWithEmailAndPassword(email, password) //this functino returns the credentials of a user
         .then((cred) => {
             console.log(cred);
 
-            const modal = document.querySelector('#modal-signup')
+            const modal = document.querySelector("#modal-signup");
             M.Modal.getInstance(modal).close();
             signup.reset();
         })
         .catch((err) => {
             console.log(err);
         });
-
 });
 
-//logout 
+//logout
 
-const logout = document.querySelector('#logout')
-logout.addEventListener('click',e=>{
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
     e.preventDefault();
 
     //Signout method
-    auth.signOut().then(()=>{//signout is a async function
+    auth.signOut().then(() => {
+        //signout is a async function
 
         console.log("The user has signed out");
     });
-})
-
+});
 
 //LOGING IN A PERSON
-const login = document.querySelector('#login-form');
+const login = document.querySelector("#login-form");
 
-login.addEventListener('submit',e=>{
+login.addEventListener("submit", (e) => {
     e.preventDefault();
-    const email = login['login-email'].value;
-    const password = login['login-password'].value;
+    const email = login["login-email"].value;
+    const password = login["login-password"].value;
 
     //LOGING IN
-    auth.signInWithEmailAndPassword(email,password).then(cred =>{
-        console.log(cred,cred.user,cred.uid);
-    }).catch(err=>{
-        console.log(err);
-    });
+    auth.signInWithEmailAndPassword(email, password)
+        .then((cred) => {
+            console.log(cred, cred.user, cred.uid);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 
     const modal = document.querySelector("#modal-login");
     M.Modal.getInstance(modal).close();
-    login.reset()
-
-})
+    login.reset();
+});
--- Firebase_Auth/scripts/index.js
+++ Firebase_Auth/scripts/index.js
@@ -1,3 +1,33 @@
+const guideList = document.querySelector(".guides");
+const logout_list = document.querySelectorAll('.logged-out');
+const login_list = document.querySelectorAll('.logged-in');
+
+const setupUI= user=>{
+    if (user){//if were logged in we show certain links
+        login_list.forEach((item) => (item.style.display = "block"));
+        logout_list.forEach((item) => (item.style.display = "none"));
+    }
+    else{//we are logged out and show certain links
+        login_list.forEach((item) => (item.style.display = "none"));
+        logout_list.forEach((item) => (item.style.display = "block"));
+    }
+}
+const setupGuides = (data) => {
+    let html = ``;
+    data.forEach((doc) => {
+        const guide = doc.data();
+        const li = `
+        <li>
+            <div class="collapsible-header grey lighten-4">${guide.title}</div>
+            <div class="collapsible-body white">${guide.content}</div>
+        </li>
+        `;
+        html += li;
+    });
+    guideList.innerHTML = html;
+    
+};
+
 // setup materialize components
 document.addEventListener("DOMContentLoaded", function () {
     var modals = document.querySelectorAll(".modal");
